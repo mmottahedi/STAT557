2ec3d5eef5b4396bffef396c799cc60aa21771db
mmottahedi <m.mottahedi@gmail.com>
2016-02-10 17:20:06 -0500

    LDA and QDA completed, jupyter notebook added for testing and visualization

--- project1/LDA.py
+++ project1/LDA.py
@@ -1,21 +1,19 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import scipy as sp
-from scipy.linalg import eig
-
 """
 QDA and LDA for Stat 557
 
 """
 
+import numpy as np
+from scipy.linalg import eig, inv
+from numpy import diag, real, transpose, log, dot
+
+
 class DA(object):
     """
     Linear Discriminant Analysis
     """
-    import numpy as np
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, mode="LDA"):
         """
         inputs:
             x: matrix of predictor
@@ -23,77 +21,115 @@ class DA(object):
         """
         self.x = x
         self.y = y
+        self.mode = mode
         self.xDim = np.shape(x)
         self.yDim = np.shape(y)
         self.classSize = len(set(y))
         self.classMember = set(y)
+        self.classMean = {}
+        self.priorProbability = {}
+        self.classCovMatrix = {}
+        self.commonCovMatrix = None
 
-    def preprocessing(self):
+    def sampleEstimate(self):
         """
         compute the mean and covariance matrix and class prior probability
         """
-        self.classMean = {}
-        self.priorProbability = {}
-
         for K in self.classMember:
-
-            classK_data = self.x[self.y == K,]
-            self.classMean[K] = np.transpose(np.mean(classK_data,0))
-
+            classK_data = self.x[self.y == K, ]
+            self.classMean[K] = np.transpose(np.mean(classK_data, 0))
             self.priorProbability[K] = sum(self.y == K) / len(self.y)
-
+            self.classCovMatrix[K] = np.cov(np.transpose(classK_data))
         self.commonCovMatrix = np.cov(np.transpose(self.x))
 
-
-
-
-
     def LDA(self, x):
+        """
+        inputs:
+            x: numpy array
+        output:
+            delta_k: dictionary detla for each class
+        """
+        self.sampleEstimate()
         delta_k = {}
         for K in self.classMember:
             delta_k[K] = np.dot(np.dot(x, self.commonCovMatrix),
-                    self.classMean[K]) - .5 * np.dot(
-                            np.dot(np.transpose(self.classMean[K]),
-                                self.commonCovMatrix),
-                   self.classMean[K]) + np.log(self.priorProbability[K])
-        return max(delta_k, key = delta_k.get)
+                                self.classMean[K]) - .5 * np.dot(
+                                np.dot(np.transpose(self.classMean[K]),
+                                       inv(self.commonCovMatrix)),
+                                self.classMean[K]) +\
+                np.log(self.priorProbability[K])
+        print("LDA delta_k", delta_k)
+        return max(delta_k, key=delta_k.get)
 
-    def QDA():
-        pass
+    def eigDecomp(self):
+        """
+        eigen value decomposition for each class
+        returns dictionary of eig vectors and eigen values for each class
+        """
+        eigen_dict = {}
+        for key in self.classCovMatrix:
+            eigen_dict[key] = eig(self.classCovMatrix[key])
+        return eigen_dict
 
-    def fit(self, mode):
-        self.y_hat = []
-        if mode == "LDA":
-            for X in self.x:
-                self.y_hat.append(self.LDA(X))
-        else:
-            pass
-        return self.y_hat
+    def QDA(self, x):
+        """
+        inputs:
+            x: numpy array
+        output:
+            maximum delta between classes
+        """
+        delta_k = {}
+        self.sampleEstimate()
+        eigen_dict = self.eigDecomp()
+        for K in self.classMember:
+            D_k = diag(real(eigen_dict[K][0]))
+            U_k = eigen_dict[K][1]
+            log_cov = sum(diag(D_k))
+            part2 = dot(dot(transpose(dot(U_k.T,
+                                      (x - self.classMean[K]))), inv(D_k)),
+                        (dot(U_k.T, (x - self.classMean[K]))))
+            part3 = log(self.priorProbability[K])
+
+            delta_k[K] = -0.5 * log_cov - 0.5 * part2 + part3
+        return max(delta_k, key=delta_k.get)
 
     def summary(self):
         self.accuracy = sum(self.y_hat == self.y) / len(self.y)
         print("accuracy: ", self.accuracy)
 
+    def predict(self, X):
+        """
+        returns the predicted class labels
+        input: np array
+        output: numpy array
+        """
+        self.y_hat = []
+        if self.mode == "LDA":
+            for X in self.x:
+                self.y_hat.append(self.LDA(X))
+        if self.mode == "QDA":
+            for X in self.x:
+                self.y_hat.append(self.QDA(X))
+        return self.y_hat
 
 
 if __name__ == "__main__":
     np.random.seed(1234)
-    cov = [[1.79, -0.14],[-.14,1.66]]
-    mean1 = [0,0]
-    mean2 = [2,2]
+    cov = [[5.79, -0.14], [-.14, 1.66]]
+    mean1 = [0, 0]
+    mean2 = [2, 2]
     Y = np.random.randint(0, 2, 1000)
     X1 = np.random.multivariate_normal(mean1, cov, 475)
     X2 = np.random.multivariate_normal(mean2, cov, 525)
-    X = np.zeros((1000,2))
-    X[Y==0] = X1
-    X[Y==1] = X2
-    print(Y)
-    fit = DA(X, Y)
+    X = np.zeros((1000, 2))
+    X[Y == 0] = X1
+    X[Y == 1] = X2
+    fit = DA(X, Y, mode="QDA")
     print(fit.xDim, fit.yDim)
-    fit.preprocessing()
+    fit.sampleEstimate()
     print(fit.classMean)
-    print(fit.priorProbability)
-    print(fit.commonCovMatrix)
-    print(np.shape(fit.commonCovMatrix))
-    print(fit.fit("LDA"))
+    print("priorProbability: \n ", fit.priorProbability)
+    print("common covariance matrix: \n ", fit.commonCovMatrix)
+    print(type(fit.commonCovMatrix))
+    y_hat = fit.predict(X)
     fit.summary()
